dafd41032cbdff4882bd1bd2ed579f67b3e29e99
Oluwoye <Tim.Cech@student.hpi.uni-potsdam.de>
2017-10-28 17:10:21 +0200

    New author page should render. Closes #1

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  resources :authors
   root 'home#index'
  
   # Example of regular route:

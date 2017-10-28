99626fc98f3ed27f870572c6e6381a3a37308849
Oluwoye <Tim.Cech@student.hpi.uni-potsdam.de>
2017-10-28 22:41:12 +0200

    Author has firstname, lastname, and homepage. Closes #4

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,7 +4,7 @@ describe Author, :type => :model do
   it "Should create Alan Turing" do
    # visit new_author_path
 
-    author = Author.new(First_name: "Alan", Last_name: "Turing", Homepage: "ttp://wikipedia.org/Alan_Turning")
+    author = Author.new(First_name: "Alan", Last_name: "Turing", Homepage: "http://wikipedia.org/Alan_Turning")
     expect(author.First_name).to eq("Alan")
     expect(author.Last_name).to eq("Turing")
     expect(author.Homepage).to eq("http://wikipedia.org/Alan_Turning")

a949292c93a34836c9228720d4829ec7d3f225bc
Oluwoye <Tim.Cech@student.hpi.uni-potsdam.de>
2017-10-28 17:21:58 +0200

    failing test for #2

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,11 @@ describe "New author page", type: :feature do
   it "should render withour error" do
     visit new_author_path
   end
+  it "should have content" do
+    visit new_author_path
+    
+    expect(page).to have_field('First name')
+    expect(page).to have_field('Last name')
+    expect(page).to have_field('Homepage')
+  end
 end

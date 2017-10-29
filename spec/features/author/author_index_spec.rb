17f683394cd7f3afd04f61d73942d47285a9c232
Oluwoye <Tim.Cech@student.hpi.uni-potsdam.de>
2017-10-29 13:40:00 +0100

    Failing test for #14

--- spec/features/author/author_index_spec.rb
+++ spec/features/author/author_index_spec.rb
@@ -5,4 +5,9 @@ describe "Author index page", type: :feature do
   it "Should render index page" do
 	visit '/authors'
   end
-end 
+  it "Should have name column" do
+	visit 'authors'
+
+ 	expect(page).to have_css("name", :count => 1)	
+  end
+end

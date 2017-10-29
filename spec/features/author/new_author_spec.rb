9364c354e2e41bc3c6db6c3d7a64729b514cbf8e
Oluwoye <Tim.Cech@student.hpi.uni-potsdam.de>
2017-10-29 11:35:14 +0100

    Fix namespace. Closes #9

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -8,16 +8,16 @@ describe "New author page", type: :feature do
   it "should have content" do
     visit new_author_path
     
-    expect(page).to have_field('Author first name')
-    expect(page).to have_field('Author last name')
-    expect(page).to have_field('Author homepage')
+    expect(page).to have_field('First name')
+    expect(page).to have_field('Last name')
+    expect(page).to have_field('Homepage')
   end
   it "should save author" do
     visit new_author_path
 
-    fill_in "Author first name", :with => "Alan"
-    fill_in "Author last name", :with => "Turing"
-    fill_in "Author homepage", :with => "http://wikipedia.org/Alan_Turing"
+    fill_in "First name", :with => "Alan"
+    fill_in "Last name", :with => "Turing"
+    fill_in "Homepage", :with => "http://wikipedia.org/Alan_Turing"
     find('input[type="submit"]').click
   end
 end

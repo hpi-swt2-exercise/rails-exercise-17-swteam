fc1c9e4c09ce9b6ef1e2af9a554cc98bd962d0ec
Oluwoye <Tim.Cech@student.hpi.uni-potsdam.de>
2017-10-29 13:49:15 +0100

    Index page has column Name. Closes #14

--- spec/features/author/author_index_spec.rb
+++ spec/features/author/author_index_spec.rb
@@ -8,6 +8,6 @@ describe "Author index page", type: :feature do
   it "Should have name column" do
 	visit 'authors'
 
- 	expect(page).to have_css("name", :count => 1)	
+ 	expect(page).to have_content("Name", :count => 1)	
   end
 end

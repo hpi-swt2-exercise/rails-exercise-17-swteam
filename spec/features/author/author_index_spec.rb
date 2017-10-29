a51f0e5b2f0be6dd4de7e8dbf76055e978cad50b
Oluwoye <Tim.Cech@student.hpi.uni-potsdam.de>
2017-10-29 13:57:07 +0100

    Failing test for #15

--- spec/features/author/author_index_spec.rb
+++ spec/features/author/author_index_spec.rb
@@ -6,8 +6,13 @@ describe "Author index page", type: :feature do
 	visit '/authors'
   end
   it "Should have name column" do
-	visit 'authors'
+	visit '/authors'
 
  	expect(page).to have_content("Name", :count => 1)	
+  end
+  it "Should have link to new author page" do
+	visit '/authors'
+
+	expect(page).to have_css("p a", :text => "Add author")
   end
 end

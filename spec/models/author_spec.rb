bde3f063a2262f8c899a1c95092cdb185435f6e6
Oluwoye <Tim.Cech@student.hpi.uni-potsdam.de>
2017-10-31 00:03:11 +0100

    Author mustn't validate without last name. Closes #18

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -10,4 +10,9 @@ describe Author, :type => :model do
     expect(author.homepage).to eq("http://wikipedia.org/Alan_Turning")
     expect(author.name).to eq("Alan Turing")
   end
+
+  it "Should ensure last name" do
+    author = Author.new(first_name: "Alan", homepage: "http://wikipedia.org/Alan_Turning")
+    expect(author.id).to be_nil
+  end
 end

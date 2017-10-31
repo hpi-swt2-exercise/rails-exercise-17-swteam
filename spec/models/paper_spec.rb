2fbb83876a576778bcbb9e97b97d3d6d1a859e7e
Oluwoye <Tim.Cech@student.hpi.uni-potsdam.de>
2017-10-31 20:07:26 +0100

    Paper mustn't validate without title. Closes #26

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Paper, :type => :model do
+  it "Should create paper" do
+   # visit new_author_path
+
+    paper = Paper.new(title: "MACHINE LEARNING EXTREME", venue: "mind 49: 433-460", year: 1950)
+    expect(paper.title).to eq("MACHINE LEARNING EXTREME")
+    expect(paper.venue).to eq("mind 49: 433-460")
+    expect(paper.year).to eq(1950)
+   # expect(author.name).to eq("Alan Turing")
+  end
+
+  it "Should ensure last name" do
+    paper = Paper.new(venue: "mind 49: 433-460", year: 1950)
+    expect(paper.id).to be_nil
+  end
 end
